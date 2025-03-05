bda423edb38d736e6900c3a537b00d9b5b3b8189
greg-tucker <greg.otherworld@gmail.com>
2025-03-05 20:42:37 +0000

    Added spring folder

---
+++ spring/src/test/java/com/lemonfungus/RankedRace/RankedRaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemonfungus.RankedRace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankedRaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
