5e52d03899524ee3def2f6e73c073bc3b1b5a3e7
ziqinwong <ziqin.wong@gmail.com>
2025-03-09 13:55:02 +0000

    added epic music

--- react/ranked-race/src/app/page.tsx
+++ react/ranked-race/src/app/page.tsx
@@ -1,17 +1,45 @@
 'use client';
 
+import AudioPlayer from '@/components/AudioPlayer';
 import { HeaderMenu } from '@/components/HeaderMenu';
 import { MainRankings } from '@/components/MainRankings';
+import { Modal } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { useRef } from 'react';
 
 export default function HomePage() {
+  const [opened, { close }] = useDisclosure(true);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
   return (
     <>
-      <HeaderMenu />
-      <main>
-        <h3>Most Epic Ranking!</h3>
-        <MainRankings />
-      </main>
-      <footer>Brought to you by Greg, Zi, Nikki</footer>
+      <audio loop ref={audioRef} id="gods">
+        <source src="/audio/gods.mp3" type="audio/mpeg" />
+        Gods
+      </audio>
+
+      <Modal
+        opened={opened}
+        onClose={close}
+        withCloseButton={false}
+        fullScreen
+        centered
+        transitionProps={{ transition: 'fade', duration: 200 }}
+      >
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
+          <AudioPlayer closeModal={close} audioRef={audioRef} text='ARE YOU READY???' />
+        </div>
+      </Modal>
+
+      <div>
+        <HeaderMenu />
+        <main>
+          <h3>Most Epic Ranking!</h3>
+          <MainRankings />
+        </main>
+        <footer>Brought to you by Greg, Zi, Nikki</footer>
+        <AudioPlayer closeModal={close} audioRef={audioRef} text='Toggle music' />
+      </div>
     </>
   );
 }
---
+++ react/ranked-race/src/components/AudioPlayer.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@mantine/core';
+import React from 'react';
+
+interface AudioPlayerProps {
+  closeModal: () => void; 
+  audioRef: React.RefObject<HTMLAudioElement>; 
+  text: string;
+
+}
+
+export default function AudioPlayer({ closeModal, audioRef, text }: AudioPlayerProps) {
+
+  const playSong = () => {
+    if (audioRef.current) { // Optional chaining to avoid errors
+      if (audioRef.current.paused) {
+        audioRef.current.play().catch((err) => console.error('Play failed:', err)); // Handle play errors
+      } else {
+        audioRef.current.pause();
+      }
+    } else {
+      console.error("audioRef is null");
+    }
+
+    closeModal();
+  };
+
+  return (
+    <>
+      <Button onClick={playSong} mx="auto" mt="xl">{text}</Button>
+    </>
+  );
+}
