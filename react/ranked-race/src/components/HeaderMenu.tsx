337b8665544c1556bf28ae617a578aac84234ca9
greg-tucker <greg.otherworld@gmail.com>
2026-02-21 21:12:16 +0000

    question?

--- react/ranked-race/src/components/HeaderMenu.tsx
+++ react/ranked-race/src/components/HeaderMenu.tsx
@@ -15,7 +15,7 @@ export function HeaderMenu() {
       <div>
         <div className="rr-title">Ranked Race</div>
         <div style={{ color: '#a8b5d8', fontSize: 18, fontWeight: 400, marginTop: 2, letterSpacing: 1 }}>
-          你好 你要回我的家
+          你好 你要回我的家吗
         </div>
       </div>
     </header>
